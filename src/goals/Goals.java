6d256fc8ccbe325464bc8095c164528e932b131e
errorc2146 <errorc2146@nigge.rs>
2016-08-11 09:34:24 +0100

    About to implement browsing

--- src/goals/Goals.java
+++ src/goals/Goals.java
@@ -1,21 +1,9 @@
 package goals;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import javax.swing.border.*;
+import java.awt.EventQueue;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.util.zip.*;
-
-public class Goals extends JFrame
-{
-    GoalJList list;
-    JTextArea todaysEntry;
-    String currentFile;
-    SaveFileManager sfm;
-    
+public class Goals
+{    
     public static void main(String[] args) 
     {        
         EventQueue.invokeLater(new Runnable() 
@@ -23,171 +11,10 @@ public class Goals extends JFrame
             @Override
             public void run() 
             {
-                Goals window = new Goals();
+                GoalsWindow window = new GoalsWindow(new ShortDate());
                 window.setVisible(true);
             }
         });
-    }
-    
-    public Goals()
-    {
-        list = new GoalJList();
-        sfm = new SaveFileManager();
-        currentFile = null;
-             
-        initUI();
-    }
-    
-    private void initUI()
-    {
-        setTitle("Goals");
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setPreferredSize(new Dimension(1000,600));
-        
-        todaysEntry = new JTextArea(5, 100);
-        todaysEntry.setLineWrap(true);
-        todaysEntry.setWrapStyleWord(true);
-        JScrollPane notesContainer = new JScrollPane(todaysEntry);
-        
-        notesContainer.setBorder(
-            BorderFactory.createTitledBorder(
-                new LineBorder(Color.GRAY),
-                "Notes for " + new ShortDate() +":"));
-        
-        //list for goals
-        JScrollPane listContainer = new JScrollPane(list);
-        listContainer.setBorder(
-            BorderFactory.createTitledBorder(
-                new LineBorder(Color.GRAY),
-                "Ongoing goals:"));
-        
-        notesContainer.setPreferredSize(new Dimension(640, 340));
-        listContainer.setPreferredSize(new Dimension(640, 140));
-        JSplitPane split = new JSplitPane(JSplitPane.VERTICAL_SPLIT, notesContainer, listContainer);
-        split.setResizeWeight(0.6);
-        getContentPane().add(split);
-        
-        JMenuBar menuBar = new JMenuBar();
-        JMenu file = new JMenu("File");
-        
-        JMenuItem load = new JMenuItem("Load");
-        load.addActionListener(new ActionListener()
-        {
-           public void actionPerformed(ActionEvent e)
-           {
-                sfm.load(list, todaysEntry);
-           }
-        });
-        file.add(load);
-        
-        JMenuItem save = new JMenuItem("Save");
-        save.addActionListener(new ActionListener()
-        {
-           public void actionPerformed(ActionEvent e)
-           {
-               sfm.save(list, todaysEntry.getText());
-           }
-        });
-        file.add(save);
-        
-        JMenuItem saveas = new JMenuItem("Save as");
-        saveas.addActionListener(new ActionListener()
-        {
-           public void actionPerformed(ActionEvent e)
-           {
-                sfm.saveas(list, todaysEntry.getText());
-           }
-        });
-        file.add(saveas);
-        
-        menuBar.add(file);
-        setJMenuBar(menuBar);
-        
-        //List right click menu
-        JMenuItem newG = new JMenuItem("New");
-        newG.addActionListener(new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                NewGoalDialog ngd = new NewGoalDialog();
-                ngd.setVisible(true);
-                
-                if (ngd.getCreatedGoal() != null)
-                    list.addGoal(ngd.getCreatedGoal());
-                
-                ngd.dispose();
-                
-            }
-        });
-        
-        JMenuItem accomplished = new JMenuItem("Finished");
-        
-        JMenuItem edit = new JMenuItem("Edit");
-        edit.addActionListener(new ActionListener()
-        {
-           @Override
-           public void actionPerformed(ActionEvent e)
-           {
-                Goal remove = (Goal)list.getSelectedValue(); 
-
-                NewGoalDialog ngd = new NewGoalDialog(remove);
-                ngd.setVisible(true);
-
-                if (ngd.getCreatedGoal() != null)
-                {
-                    list.deleteGoal(remove);
-                    list.addGoal(ngd.getCreatedGoal());
-                }
-                ngd.dispose();
-           }
-        });
-        
-        JMenuItem del = new JMenuItem("Delete");
-        del.addActionListener(new ActionListener()
-        {
-           @Override
-           public void actionPerformed(ActionEvent e)
-           {
-               list.deleteSelectedFiles();
-           }
-        });
-        
-        JPopupMenu popupMenu = new JPopupMenu();
-        popupMenu.add(newG);
-        popupMenu.add(accomplished);
-        popupMenu.add(edit);
-        popupMenu.add(del);
-        
-        list.addMouseListener(new MouseAdapter()
-        {
-            @Override
-            public void mousePressed(MouseEvent e)
-            {
-                if (SwingUtilities.isRightMouseButton(e))
-                {
-                    JList list = (JList)e.getSource();
-                    
-                    int selectedGoals = list.getSelectedIndices().length;
-                    if (selectedGoals < 2)
-                    {
-                        int row = list.locationToIndex(e.getPoint());
-                        list.setSelectedIndex(row);
-                    }
-                    
-                    boolean itemSelected = selectedGoals > 0;
-                    edit.setEnabled(itemSelected);
-                    accomplished.setEnabled(itemSelected);
-                    del.setEnabled(itemSelected);
-                    
-                    popupMenu.show(list, e.getX(), e.getY());
-                }
-            }
-
-        });
-        
-        pack();
     }
 }
 
